5902a2c5fd5e7853e501d082a298b641676b650b
boleksy <boleksy@ucsc.edu>
2015-02-06 18:02:50 -0800

    Version of snake game attempt using Brine.
    Brine is useless and a piece of shit. Don't use it.

--- HW2_snake_boleksy.js
+++ HW2_snake_boleksy.js
@@ -18,18 +18,19 @@ function initSnake(){
 	snakeArray = [];
 	
 	//creates snake in top left
-	for(var i = size - 1; i >= 0; i--)
+	for(var i = size - 1; i >= 0; i--){
 		snakeArray.push({x: i, y: 0});
+	}
 		
 }
 
 function fillSnake(){
 	for(var i = 0; i < snakeArray.length; i++){
 		var cell = snakeArray[i];
-		ctx.fillStyle("green");
-		ctx.fillRect(cell.x*10, cell.y*10, 10, 10);
-		ctx.fillStyle("red");
-		ctx.fillRect(cell.x*10, cell.y*10, 10, 10);
+		fillStyle("green");
+		fillRect(cell.x*10, cell.y*10, 10, 10);
+		fillStyle("red");
+		fillRect(cell.x*10, cell.y*10, 10, 10);
 	}
 }
 
