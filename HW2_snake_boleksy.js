4851aad05d88ea17e7b41c2b0970d1aaa53bc00f
boleksy <boleksy@ucsc.edu>
2015-02-06 17:45:14 -0800

    Files for snake game first commited
    brin.js canvas.html HW2_snake_boleksy.js are commited

---
+++ HW2_snake_boleksy.js
@@ -0,0 +1,36 @@
+//Make sure that 2D
+use2D = true;
+
+//initialize game canvas
+initGame("canvas");
+
+//array to be used for snake cells
+var snakeArray;
+
+initSnake();
+
+function initSnake(){
+
+	//initial length of snake
+	var size = 5;
+	
+	//start with empty array for snake to be filled
+	snakeArray = [];
+	
+	//creates snake in top left
+	for(var i = size - 1; i >= 0; i--)
+		snakeArray.push({x: i, y: 0});
+		
+}
+
+function fillSnake(){
+	for(var i = 0; i < snakeArray.length; i++){
+		var cell = snakeArray[i];
+		ctx.fillStyle("green");
+		ctx.fillRect(cell.x*10, cell.y*10, 10, 10);
+		ctx.fillStyle("red");
+		ctx.fillRect(cell.x*10, cell.y*10, 10, 10);
+	}
+}
+
+fillSnake();
