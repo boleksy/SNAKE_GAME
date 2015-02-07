96ea2df1d9706c7a3c4e4ad341fd7f6fbd2765a6
boleksy <boleksy@ucsc.edu>
2015-02-06 20:47:24 -0800

    Basic snake game
    Attempting to make snake faster

--- HW2_snake_boleksy.js
+++ HW2_snake_boleksy.js
@@ -1,10 +1,10 @@
 
-var COLS = 26;
-var ROWS = 26;
+var columns = 30;
+var rows = 30;
 
-var EMPTY = 0;
-var SNAKE = 1;
-var FRUIT = 2;
+var emptyFill = 0;
+var snakeFill = 1;
+var foodFill = 2;
 
 var LEFT = 0;
 var UP = 1;
@@ -15,14 +15,13 @@ var KEY_LEFT = 37;
 var KEY_UP = 38;
 var KEY_RIGHT = 39;
 var KEY_DOWN = 40;
-/**
-* Game objects
-*/
-var canvas; /* HTMLCanvas */
-var ctx; /* CanvasRenderingContext2d */
-var keystate; /* Object, used for keyboard inputs */
-var frames; /* number, used for animation */
-var score; /* number, keep track of the player score */
+
+//objects
+var canvas; 
+var ctx; 
+var keystate; 
+var frames; 
+var score; 
 /**
 * Grid datastructor, usefull in games where the game world is
 * confined in absolute sized chunks of data or information.
@@ -30,249 +29,234 @@ var score; /* number, keep track of the player score */
 * @type {Object}
 */
 grid = {
-width: null, /* number, the number of columns */
-height: null, /* number, the number of rows */
-_grid: null, /* Array<any>, data representation */
-/**
-* Initiate and fill a c x r grid with the value of d
-* @param {any} d default value to fill with
-* @param {number} c number of columns
-* @param {number} r number of rows
-*/
-init: function(d, c, r) {
-this.width = c;
-this.height = r;
-this._grid = [];
-for (var x=0; x < c; x++) {
-this._grid.push([]);
-for (var y=0; y < r; y++) {
-this._grid[x].push(d);
+	width: null, /* number, the number of columns */
+	height: null, /* number, the number of rows */
+	_grid: null, /* Array<any>, data representation */
+
+	//function to call that creates snake grid
+	init: function(d, c, r) {
+		this.width = c;
+		this.height = r;
+		this._grid = [];
+		
+		for (var x=0; x < c; x++) {
+			this._grid.push([]);
+			for (var y=0; y < r; y++) {
+				this._grid[x].push(d);
+			}
+		}
+	},
+
+	//creates values for cells in grid
+	set: function(val, x, y) {
+		this._grid[x][y] = val;
+	},
+
+//gets values of the cell at point in grid
+	get: function(x, y) {
+		return this._grid[x][y];
+	}
+	
 }
-}
-},
-/**
-* Set the value of the grid cell at (x, y)
-*
-* @param {any} val what to set
-* @param {number} x the x-coordinate
-* @param {number} y the y-coordinate
-*/
-set: function(val, x, y) {
-this._grid[x][y] = val;
-},
-/**
-* Get the value of the cell at (x, y)
-*
-* @param {number} x the x-coordinate
-* @param {number} y the y-coordinate
-* @return {any} the value at the cell
-*/
-get: function(x, y) {
-return this._grid[x][y];
-}
-}
-/**
-* The snake, works as a queue (FIFO, first in first out) of data
-* with all the current positions in the grid with the snake id
-*
-* @type {Object}
-*/
+
+//snake as queue object to keep trake of current snake positions
 snake = {
-direction: null, /* number, the direction */
-last: null, /* Object, pointer to the last element in
-the queue */
-_queue: null, /* Array<number>, data representation*/
-/**
-* Clears the queue and sets the start position and direction
-*
-* @param {number} d start direction
-* @param {number} x start x-coordinate
-* @param {number} y start y-coordinate
-*/
-init: function(d, x, y) {
-this.direction = d;
-this._queue = [];
-this.insert(x, y);
-},
-/**
-* Adds an element to the queue
-*
-* @param {number} x x-coordinate
-* @param {number} y y-coordinate
-*/
-insert: function(x, y) {
-// unshift prepends an element to an array
-this._queue.unshift({x:x, y:y});
-this.last = this._queue[0];
-},
-/**
-* Removes and returns the first element in the queue.
-*
-* @return {Object} the first element
-*/
-remove: function() {
-// pop returns the last element of an array
-return this._queue.pop();
-}
+	direction: null, //direction #
+	last: null, //keeps track of lat element in queue
+	_queue: null, //queue to hold snake
+
+	//snake starting position
+	init: function(d, x, y) {
+		this.direction = d;
+		this._queue = [];
+		this.insert(x, y);
+	},
+
+	//makes snake longer
+	insert: function(x, y) {
+	
+		//adds element to the end of snake
+		this._queue.unshift({x:x, y:y});
+	
+		this.last = this._queue[0];
+	},
+
+	//removes top of snake for movement
+	remove: function() {
+		return this._queue.pop();
+	}
+	
 };
-/**
-* Set a food id at a random free cell in the grid
-*/
+
+//runs game
+main();
+
+//randomly sets position for food
 function setFood() {
-var empty = [];
-// iterate through the grid and find all empty cells
-for (var x=0; x < grid.width; x++) {
-for (var y=0; y < grid.height; y++) {
-if (grid.get(x, y) === EMPTY) {
-empty.push({x:x, y:y});
+	var empty = [];
+	
+	//finds all empty cells so not to collide with snake
+	for (var x=0; x < grid.width; x++) {
+		for (var y=0; y < grid.height; y++) {
+			if (grid.get(x, y) === emptyFill) {
+				empty.push({x:x, y:y});
+			}
+		}
+	}
+	
+	//puts food in random cell
+	var randpos = empty[Math.round(Math.random()*(empty.length - 1))];
+	grid.set(foodFill, randpos.x, randpos.y);
 }
-}
-}
-// chooses a random cell
-var randpos = empty[Math.round(Math.random()*(empty.length - 1))];
-grid.set(FRUIT, randpos.x, randpos.y);
-}
-/**
-* Starts the game
-*/
+
+//initial game position
 function main() {
-// create and initiate the canvas element
-canvas = document.createElement("canvas");
-canvas.width = COLS*20;
-canvas.height = ROWS*20;
-ctx = canvas.getContext("2d");
-// add the canvas element to the body of the document
-document.body.appendChild(canvas);
-// sets an base font for bigger score display
-ctx.font = "12px Helvetica";
-frames = 0;
-keystate = {};
-// keeps track of the keybourd input
-document.addEventListener("keydown", function(evt) {
-keystate[evt.keyCode] = true;
-});
-document.addEventListener("keyup", function(evt) {
-delete keystate[evt.keyCode];
-});
-// intatiate game objects and starts the game loop
-init();
-loop();
+	//create game canvas
+	canvas = document.createElement("canvas");
+	canvas.width = columns*10;
+	canvas.height = rows*10;
+	
+	//make sure canvas is 2D
+	ctx = canvas.getContext("2d");
+	
+	//add canvas to world
+	document.body.appendChild(canvas);
+	
+	//score display
+	ctx.font = "18px Times New Roman";
+	
+	//frame count initialized
+	frames = 0;
+	keystate = {};
+	
+	//keeps track of input from user
+	document.addEventListener("keydown", function(evt) {keystate[evt.keyCode] = true;});
+	document.addEventListener("keyup", function(evt) {delete keystate[evt.keyCode];}); // <-- forgot first semicolon here. drove me crazy for hours
+
+	//initialize game
+	init();
+	
+	//loop game
+	loop();
 }
-/**
-* Resets and inits game objects
-*/
+
+//resets and initializes game objects in beginning and after death
 function init() {
-score = 0;
-grid.init(EMPTY, COLS, ROWS);
-var sp = {x:Math.floor(COLS/2), y:ROWS-1};
-snake.init(UP, sp.x, sp.y);
-grid.set(SNAKE, sp.x, sp.y);
-setFood();
+	score = 0;
+	grid.init(emptyFill, columns, rows);
+	var sp = {x:Math.floor(columns/2), y:rows-1};
+	snake.init(UP, sp.x, sp.y);
+	grid.set(snakeFill, sp.x, sp.y);
+	setFood();
 }
-/**
-* The game loop function, used for game updates and rendering
-*/
+
+//update and renders game
 function loop() {
-update();
-draw();
-// When ready to redraw the canvas call the loop function
-// first. Runs about 60 frames a second
-window.requestAnimationFrame(loop, canvas);
+	update();
+	draw();
+	
+	//calls loop when canvas is ready to be drawn again
+	window.requestAnimationFrame(loop, canvas);
 }
 /**
 * Updates the game logic
 */
 function update() {
-frames++;
-// changing direction of the snake depending on which keys
-// that are pressed
-if (keystate[KEY_LEFT] && snake.direction !== RIGHT) {
-snake.direction = LEFT;
+	var speed = 10;
+	frames++;
+
+//changes snake direction
+//However, cannot turn-around when opposite key of previous keypressed is pressed
+	if (keystate[KEY_LEFT] && snake.direction != RIGHT) {
+		snake.direction = LEFT;
+	}
+	if (keystate[KEY_UP] && snake.direction != DOWN) {
+		snake.direction = UP;
+	}
+	if (keystate[KEY_RIGHT] && snake.direction != LEFT) {
+		snake.direction = RIGHT;
+	}
+	if (keystate[KEY_DOWN] && snake.direction != UP) {
+		snake.direction = DOWN;
+	}
+	
+	//updates speed of snake for every 3 points
+	if(score%3 == 0 && speed > 2){
+		speed--;
+	}
+	
+	//updates game and speed of snake
+	if (frames%speed == 0) {
+		
+		//pops head from queue
+		var nx = snake.last.x;
+		var ny = snake.last.y;
+
+		//moves snake position
+		switch (snake.direction) {
+			case LEFT:
+				nx--;
+				break;
+			case UP:
+				ny--;
+				break;
+			case RIGHT:
+				nx++;
+				break;
+			case DOWN:
+				ny++;
+				break;
+		}
+		
+		//checks to see if game over when snake collides with wall
+		if (0 > nx || nx > grid.width-1 || 0 > ny || ny > grid.height-1 || grid.get(nx, ny) === snakeFill) {
+			return init();
+		}
+		
+		//collision for food and snake
+		if (grid.get(nx, ny) === foodFill) {
+			score++;
+			setFood();
+		} 
+		//if no food collision, tail taken and given new position
+		else {
+			var tail = snake.remove();
+			grid.set(emptyFill, tail.x, tail.y);
+		}
+		grid.set(snakeFill, nx, ny);
+		snake.insert(nx, ny);
+	}
 }
-if (keystate[KEY_UP] && snake.direction !== DOWN) {
-snake.direction = UP;
-}
-if (keystate[KEY_RIGHT] && snake.direction !== LEFT) {
-snake.direction = RIGHT;
-}
-if (keystate[KEY_DOWN] && snake.direction !== UP) {
-snake.direction = DOWN;
-}
-// each five frames update the game state.
-if (frames%5 === 0) {
-// pop the last element from the snake queue i.e. the
-// head
-var nx = snake.last.x;
-var ny = snake.last.y;
-// updates the position depending on the snake direction
-switch (snake.direction) {
-case LEFT:
-nx--;
-break;
-case UP:
-ny--;
-break;
-case RIGHT:
-nx++;
-break;
-case DOWN:
-ny++;
-break;
-}
-// checks all gameover conditions
-if (0 > nx || nx > grid.width-1 ||
-0 > ny || ny > grid.height-1 ||
-grid.get(nx, ny) === SNAKE
-) {
-return init();
-}
-// check wheter the new position are on the fruit item
-if (grid.get(nx, ny) === FRUIT) {
-// increment the score and sets a new fruit position
-score++;
-setFood();
-} else {
-// take out the first item from the snake queue i.e
-// the tail and remove id from grid
-var tail = snake.remove();
-grid.set(EMPTY, tail.x, tail.y);
-}
-// add a snake id at the new position and append it to
-// the snake queue
-grid.set(SNAKE, nx, ny);
-snake.insert(nx, ny);
-}
-}
-/**
-* Render the grid to the canvas.
-*/
+
+//keeps grid and canvas together
 function draw() {
-// calculate tile-width and -height
-var tw = canvas.width/grid.width;
-var th = canvas.height/grid.height;
-// iterate through the grid and draw all cells
-for (var x=0; x < grid.width; x++) {
-for (var y=0; y < grid.height; y++) {
-// sets the fillstyle depending on the id of
-// each cell
-switch (grid.get(x, y)) {
-case EMPTY:
-ctx.fillStyle = "#fff";
-break;
-case SNAKE:
-ctx.fillStyle = "#0ff";
-break;
-case FRUIT:
-ctx.fillStyle = "#f00";
-break;
+	
+	//checks width and height of grid sections
+	var tw = canvas.width/grid.width;
+	var th = canvas.height/grid.height;
+	
+	//draws in sections of grid
+	for (var x=0; x < grid.width; x++) {
+		for (var y=0; y < grid.height; y++) {
+		//checks grid section for proper fill
+			switch (grid.get(x, y)) {
+				case emptyFill:
+					ctx.fillStyle = "#ff0000";
+					break;
+				case snakeFill:
+					ctx.fillStyle = "#000000";
+					break;
+				case foodFill:
+					ctx.fillStyle = "#00ff00";
+					break;
+			}
+		ctx.fillRect(x*tw, y*th, tw, th);
+		}
+	}
+
+	//draws score
+	ctx.fillStyle = "#000";
+	ctx.fillText("Score: " + score, 10, canvas.height-10);
 }
-ctx.fillRect(x*tw, y*th, tw, th);
-}
-}
-// changes the fillstyle once more and draws the score
-// message to the canvas
-ctx.fillStyle = "#000";
-ctx.fillText("SCORE: " + score, 10, canvas.height-10);
-}
-// start and run the game
-main();
+
+
